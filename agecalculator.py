27296e7bea020de95a92c380a71f03d11f2f9280
ahmadf21-meet <ahmadf21@meet.mit.edu>
2019-07-23 05:28:04 -0400

    Stopping here

---
+++ agecalculator.py
@@ -0,0 +1,12 @@
+age1=input("what's the age of th efirst person")
+age1=int(age1)
+age2=input("what's thhe age of the second person")
+age2=int(age2)
+print(age1)
+print(age2)
+if (age1>age2):
+    age_difference=(age1-age2)
+else :
+    age_difference=(age2-age1)
+    
+print(age_difference)
---
+++ inputnstuff.py
@@ -0,0 +1,8 @@
+age=input('write something ')
+print(age.swapcase())
+print(age.lower())
+print(age.upper())
+print(age.capitalize())
+
+
+
